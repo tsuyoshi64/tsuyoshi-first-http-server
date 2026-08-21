42352570b986f22a3990686c45b152dc81d4cc39
tsuyoshi64 <tsurutsuyoshi0604@gmail.com>
2026-08-21 09:47:38 +0700

    feat: add health check endpoint and namespace static files under /app

--- main.go
+++ main.go
@@ -7,13 +7,22 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
+	fileServer := http.FileServer(http.Dir("."))
+
+	mux.Handle("/app/", http.StripPrefix("/app", fileServer))
 
 	server := http.Server{
 		Handler: mux,
 		Addr:    ":8080",
 	}
 
+	log.Print("serving on port 8080...")
 	if err := server.ListenAndServe(); err != nil {
 		log.Fatalf("couldn't start the server: %v", err)
 	}
