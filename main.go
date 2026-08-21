c77c8f13d5c4e41b4784511e0237062b60ed255a
tsuyoshi64 <tsurutsuyoshi0604@gmail.com>
2026-08-21 10:20:06 +0700

    feat: track and expose fileserver request metrics
    - Add apiConfig with atomic fileserverHits counter
    - Implement middleware to increment hit counts on /app
    - Add /metrics and /reset endpoints

--- main.go
+++ main.go
@@ -1,21 +1,60 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
-func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
-		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("OK"))
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
 	})
+}
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+
+	hits := cfg.fileserverHits.Load()
+	fmt.Fprintf(w, "Hits: %d", hits)
+}
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+
+	cfg.fileserverHits.Store(0)
+	w.Write([]byte("OK"))
+}
+
+func (cfg *apiConfig) handlerHealth(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
+
+func main() {
+	apiCfg := &apiConfig{}
+	mux := http.NewServeMux()
+	
+	mux.HandleFunc("/healthz", apiCfg.handlerHealth)
+	mux.HandleFunc("/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("/reset", apiCfg.handlerReset)
 
 	fileServer := http.FileServer(http.Dir("."))
 
-	mux.Handle("/app/", http.StripPrefix("/app", fileServer))
+	mux.Handle(
+		"/app/",
+		apiCfg.middlewareMetricsInc(http.StripPrefix("/app", fileServer)),
+	)
 
 	server := http.Server{
 		Handler: mux,
