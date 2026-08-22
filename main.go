d03481127a2d65e5839191127041ed8a6898bf8c
tsuyoshi64 <tsurutsuyoshi0604@gmail.com>
2026-08-22 09:58:22 +0700

    feat: Add POST /api/validate_chirp endpoint
    Decode JSON request body containing chirp text, validate that
    its length is 140 characters or fewer, and respond with either
    a validation error or a success message.

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"sync/atomic"
 )
+
+///
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
@@ -45,11 +48,64 @@ func (cfg *apiConfig) handlerHealth(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("OK"))
 }
 
+func (cfg *apiConfig) handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Body string `json:"body"`
+	}
+	type validRes struct {
+		Valid bool `json:"valid"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+
+	if err := decoder.Decode(&params); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Something went wrong")
+		return
+	}
+
+	if len(params.Body) > 140 {
+		respondWithError(w, http.StatusBadRequest, "Chirp is too long")
+		return
+	}
+
+	respondWithJson(w, http.StatusOK, validRes{
+		Valid: true,
+	})
+}
+
+///
+
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	type errorRes struct {
+		Error string `json:"error"`
+	}
+	respondWithJson(w, code, errorRes{
+		msg,
+	})
+}
+
+func respondWithJson(w http.ResponseWriter, code int, payload any) {
+	dat, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("error handling json data: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(dat)
+}
+
+///
+
 func main() {
 	apiCfg := &apiConfig{}
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("GET /api/healthz", apiCfg.handlerHealth)
+	mux.HandleFunc("POST /api/validate_chirp", apiCfg.handlerValidateChirp)
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
 	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
 
